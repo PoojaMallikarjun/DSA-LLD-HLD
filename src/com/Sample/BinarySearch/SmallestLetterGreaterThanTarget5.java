66a0811af70281b2dab8d02d0463bbe69f450305
poojam4 <poojam4@deloitte.com>
2022-10-24 14:32:44 +0530

    find start and end index of occurrance of target value in an array

---
+++ src/com/Sample/BinarySearch/SmallestLetterGreaterThanTarget5.java
@@ -0,0 +1,30 @@
+package com.Sample.BinarySearch;
+
+// You are given an array of characters letters that is sorted in non-decreasing order,
+// and a character target. There are at least two different characters in letters.
+// Return the smallest character in letters that is lexicographically greater than target.
+// If such a character does not exist, return the first character in letters.
+// letters = ["c","f","j"], target = "a"
+// Output: "c"
+// Hint: same logic as finding ceiling of a number
+public class SmallestLetterGreaterThanTarget5 {
+
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'j';
+        System.out.println(findSmallestCharGreaterThanTarget(letters, target));
+    }
+
+    private static char findSmallestCharGreaterThanTarget(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target < letters[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return letters[start % letters.length];
+    }
+}
