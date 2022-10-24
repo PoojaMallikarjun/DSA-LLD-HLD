b8fa7357b32781b172e95f258a0d1adeaa6fbbb4
poojam4 <poojam4@deloitte.com>
2022-10-24 14:36:43 +0530

    refactored

--- src/com/Sample/BinarySearch/FindFirstAndLastPositionOfTargetInArray6.java
+++ src/com/Sample/BinarySearch/FindFirstAndLastPositionOfTargetInArray6.java
@@ -2,7 +2,7 @@ package com.Sample.BinarySearch;
 
 import java.util.Arrays;
 
-// Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+// Given an array of integers arr sorted in non-decreasing order, find the starting and ending position of a given target value.
 // If target is not found in the array, return [-1, -1].
 // You must write an algorithm with O(log n) runtime complexity.
 // If arr = [5,7,7,8,8,10], target = 8
