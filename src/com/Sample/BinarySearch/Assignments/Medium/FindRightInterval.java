0442e83431129c2fdce7c14ece305e629db18890
poojam4 <poojam4@deloitte.com>
2022-11-01 19:29:27 +0530

    find right interval

---
+++ src/com/Sample/BinarySearch/Assignments/Medium/FindRightInterval.java
@@ -0,0 +1,7 @@
+package com.Sample.BinarySearch.Assignments.Medium;
+
+// https://leetcode.com/problems/find-right-interval/
+// Go through 3 approaches -> Brute force, using tree map and Binary Search.
+// https://leetcode.com/problems/find-right-interval/discuss/1070955/Java-%2B-3-Easy-Solutions-%2B-Easy-to-understand-%2B-Brute-Force-%2B-TreeMap-%2B-Binary-Search
+public class FindRightInterval {
+}
